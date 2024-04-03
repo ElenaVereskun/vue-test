f43ce911765e5fe9f1be52f5ac0253b22cfc5334
ElenaVereskun <lena.vereskun@yandex.ru>
2024-04-03 18:49:11 +0300

    [+] UI

---
+++ src/components/PostForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <form class="form" @submit.prevent>
+      <h4>Добавить задачу</h4>
+      <my-input v-model="post.title" type="text" placeholder="название" />
+      <my-input v-model="post.body" type="text" placeholder="описание" />
+      <my-button @click="createPost">Добавить</my-button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        title: "",
+        body: "",
+      },
+    };
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      this.$emit("create", this.post);
+      this.post = {
+        title: "",
+        body: "",
+      };
+    },
+  },
+};
+</script>
+<style scoped>
+.form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+</style>
---
+++ src/components/UI/MyButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="btn"><slot></slot></button>
+</template>
+
+<script>
+export default {
+  name: "my-button",
+};
+</script>
+<style scoped>
+.btn {
+  margin-top: 15px;
+  padding: 10px 15px;
+  background: whitesmoke;
+  color: teal;
+  border: 1px solid teal;
+  border-radius: 10px;
+}
+</style>
